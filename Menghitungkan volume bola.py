d8252a3fc2b88eb3e4583a0c2171e46cb2cc5e25
jericohenderson <jericohenderson.id@gmail.com>
2026-04-13 20:46:31 +0700

    Create Menghitungkan volume bola.py

---
+++ Menghitungkan volume bola.py
@@ -0,0 +1,12 @@
+# Proses menghitung volume bola
+
+import math
+
+radius = float(input("Masukkan jari-jari bola"))
+
+# Rumus menghitung
+
+volume = (4/3) * 3.14 * (radius**3)
+
+# Print hasil
+print ("Volume bola adalah {volune}")
