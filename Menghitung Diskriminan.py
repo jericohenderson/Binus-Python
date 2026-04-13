bdfb7fd2168db7ce2d7a9f18fa8f777663c3f5b2
jericohenderson <jericohenderson.id@gmail.com>
2026-04-13 21:25:16 +0700

    Create Menghitung Diskriminan.py

---
+++ Menghitung Diskriminan.py
@@ -0,0 +1,23 @@
+# Menghitung diskriminan
+def calculate_discriminant()
+print("Persamaan kuadrat: ax^2 + bx + c = 0")
+#input
+a = float(input('Enter coefficient a: '))
+b = float(input('Enter coefficient b: '))
+c = float(input('Enter coefficient c: '))
+
+# Menghitung diskriminasi
+    discriminasi = (b**2) - (4*a*c)
+    
+    print(f"\nDiscriminant value: {discriminant}")
+    
+    
+    if discriminant > 0:
+        print('Two distinct real solutions.')
+    elif discriminant == 0:
+        print('One real solution (equal roots).')
+    else:
+        print('No real solutions (complex roots).')
+
+# Print function
+calculate_discriminant()
