32942f4c20c12de1241e29ac0aea05cb9cb57e6f
jericohenderson <jericohenderson.id@gmail.com>
2026-04-13 20:31:28 +0700

    Create Menghitung volume kuboid menggunakan operator aritmetika.py

---
+++ Menghitung volume kuboid menggunakan operator aritmetika.py
@@ -0,0 +1,13 @@
+# Proses untuk mmenghitungkan volume cuboid dengan operator arimatika
+
+# Input
+panjang = float(input("Masukkan panjang kuboid"))
+lebar = float(input("Masukkan lebar kuboid"))
+tinggi = float(input("Masukkan tinggi kuboid"))
+
+# Rumus menghitung volume kuboid
+
+volume = panjang * lebar * tinggi
+
+# Print hasil
+print ("Volume kuboid adalah {volume} satuan kubik")
