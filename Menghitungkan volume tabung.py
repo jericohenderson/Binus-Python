276195617eb1e6e17085da59ed7cdde92689d75b
jericohenderson <jericohenderson.id@gmail.com>
2026-04-13 20:56:21 +0700

    Create Menghitungkan volume tabung.py

---
+++ Menghitungkan volume tabung.py
@@ -0,0 +1,12 @@
+# Menghitungkan volume tabung 
+import math
+
+# Input
+radius = float(input("Masukkan jari-jari tabung"))
+tinggi = float(input("Masukkan tinggi tabung"))
+
+# Rumus
+volume = 3.14 * (radius**2) * tinggi
+
+# Print hasil 
+print("Volume tabung adalah {volume}")
