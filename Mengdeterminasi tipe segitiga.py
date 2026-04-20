bf31d29410cb9ac637484e4bb4ef4a0a215b970f
jericohenderson <jericohenderson.id@gmail.com>
2026-04-20 20:43:20 +0700

    Create Mengdeterminasi tipe segitiga.py

---
+++ Mengdeterminasi tipe segitiga.py
@@ -0,0 +1,18 @@
+side a
+side b
+side c
+
+if (a + b > c) and (a + c > b) and (b + c > a):
+  if a == b == c:
+    print ("Equilateral")
+  elif a == b or b == c or c == a
+    print ("isosceles")
+  else:
+    print ("scalene")
+
+else:
+print ("not a triangle")
+         
+           
+           
+  
