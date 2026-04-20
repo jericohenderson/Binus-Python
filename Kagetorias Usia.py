d3b51893b08002f22e6f05915d5184029cba7c66
jericohenderson <jericohenderson.id@gmail.com>
2026-04-20 19:56:09 +0700

    Create Kagetorias Usia.py

---
+++ Kagetorias Usia.py
@@ -0,0 +1,39 @@
+usia: 19
+
+if (usia <= 1):
+  print ("bayi")
+  
+elif ((usia >= 2) and (usia <= 3)):
+  print ("balita")
+
+elif ((usia >= 4) and (usia <= 5)):
+  print ("anak prasekolah")
+
+elif ((usia >= 6) and (usia <= 12)):
+  print ("anak")
+
+elif ((usia >= 13) and (usia <= 17)):
+  print ("remaja")
+
+elif ((usia >= 18) and (usia <= 21)):
+  print ("anak muda")
+
+elif ((usia >= 22) and (usia <= 30)):
+  print ("pradewasa")
+
+elif ((usia >= 31) and (usia <= 50)):
+  print ("dewasa")
+
+elif ((usia >= 51) and (usia <= 70)):
+  print ("pralansia")
+
+else:
+  print ("lansia")
+                        
+         
+
+      
+                      
+
+
+      
