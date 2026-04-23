c95c1c100013aeea831ddfceac9c1a80f280ccd2
jericohenderson <jericohenderson.id@gmail.com>
2026-04-23 20:24:53 +0700

    Update Pertemuan 4 Latihan 1.py

--- Pertemuan 4 Latihan 1.py
+++ Pertemuan 4 Latihan 1.py
@@ -1,7 +1,3 @@
 a = int(input("Enter max value: "))
-
-rows = a
-
-for i in range (rows, 0, -1):
-    for j in range(0, i):
-        print(str(i), end= ' ')
+for i in range(a, 0, -1):
+    print(str(i) * i)
