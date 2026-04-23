1387be7f0cfe0deb20ac2a9d4f153692a9486226
jericohenderson <jericohenderson.id@gmail.com>
2026-04-23 21:11:07 +0700

    Update Mengdeterminasi tipe segitiga.py

--- Mengdeterminasi tipe segitiga.py
+++ Mengdeterminasi tipe segitiga.py
@@ -1,18 +1,14 @@
-side a
-side b
-side c
+a = float(input("Enter Side A: "))
+b = float(input("Enter Side B: "))
+c = float(input("Enter Side C: "))
 
 if (a + b > c) and (a + c > b) and (b + c > a):
   if a == b == c:
       print("Equilateral")
-  elif a == b or b == c or c == a
+  elif a == b or b == c or c == a:
       print("isosceles")
   else:
       print("scalene")
 
 else:
     print("not a triangle")
-         
-           
-           
-  
