445ed647d7f25db83ebabd9c854aba63b1aec215
jericohenderson <jericohenderson.id@gmail.com>
2026-04-23 20:48:39 +0700

    Create Pertemuan 4 Latihan 3.py

---
+++ Pertemuan 4 Latihan 3.py
@@ -0,0 +1,9 @@
+status = True
+i = 0
+a = int(input("Enter a number to determine whether it's odd or even: "))
+
+if (a % 2 == 0):
+    print("This is an even number")
+else:
+    print("This is an odd number")
+    
