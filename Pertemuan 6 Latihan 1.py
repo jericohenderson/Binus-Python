b713a394452b9c5e1a9a75406490ae3ad3bbfc02
jericohenderson <jericohenderson.id@gmail.com>
2026-04-30 21:01:04 +0700

    Update Pertemuan 6 Latihan 1.py

--- Pertemuan 6 Latihan 1.py
+++ Pertemuan 6 Latihan 1.py
@@ -1,19 +1,28 @@
-class Employee:
-    "Common base class for all employees"
-    empCount = 0
-    
-    def __init__(self, name="Employee", salary=5000):
+class Student:
+    def __init__(self, name=None, score=None):
         self.name = name
-        self.salary = salary
-        Employee.empCount += 1
-        
-    def displayCount(self):
-        print("Total Employees: %d" % Employee.empCount)
-    
-    def printEmployee(self):
-        print("Name:", self.name, "\nSalary:", self.salary)
+        self.score = score
 
-employee1 = Employee("Jerico", 5000)
+    def declare_data(self, name, score):
+        self.name = name
+        self.score = score
+        print("Data Successfully Added")
 
-employee1.printEmployee()
-employee1.displayCount()
+    def display_data(self):
+        print(f"\nName: {self.name}")
+        print(f"Score: {self.score}")
+
+    def change_data(self, field, new_value):
+        if field.lower() == "name":
+            self.name = new_value
+            print("Name Data Successfully Changed")
+        elif field.lower() == "score":
+            self.score = new_value
+            print("Score Data Successfully Changed")
+        else:
+            print("Invalid field selected.")
+
+    def delete_data(self):
+        self.name = None
+        self.score = None
+        print("Data Successfully Deleted")
